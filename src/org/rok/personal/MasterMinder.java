df96380205d11c32188e5a948758f793f433b10e
Rok <rok@Roks-iMac.local>
2019-10-03 09:33:20 -0400

    Setup Provided Tests

--- src/org/rok/personal/MasterMinder.java
+++ src/org/rok/personal/MasterMinder.java
@@ -1,12 +1,23 @@
 package org.rok.personal;
 
+import java.security.InvalidParameterException;
+
 public class MasterMinder {
+    public static String BLUE = "blue";
+    public static String RED = "red";
+    public static String YELLOW = "yellow";
+
     public MasterMinder() {
 
     }
 
-    public void assignSecret(String ... pegs){
+    public void assignSecret(String ... pegs) throws InvalidParameterException {
 
+    }
+
+    public static int[] evaluate(String[] secret, String [] guess){
+        int[] result = new int[]{0,0};
+        return result;
     }
 
 }
--- test/org/rok/personal/MasterMinderTest.java
+++ test/org/rok/personal/MasterMinderTest.java
@@ -2,6 +2,8 @@ package org.rok.personal;
 
 import org.junit.jupiter.api.Test;
 
+import java.security.InvalidParameterException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MasterMinderTest {
@@ -11,9 +13,90 @@ class MasterMinderTest {
         victim = new MasterMinder();
     }
 
+//    @Test
+//    void assignSecret_NoParameters() {
+//        initVictim();
+//        try{
+//            victim.assignSecret();
+//            fail("You need at least one peg");
+//
+//        } catch (InvalidParameterException invalidPara){
+//            assertNotNull(invalidPara);
+//            assertEquals(invalidPara.getMessage(), "Some Message");
+//        }
+//    }
+//
+//    @Test
+//    void assignSecret_OnePeg() {
+//        initVictim();
+//        try{
+//            victim.assignSecret("Red");
+//            assertTrue(false);
+////            assertTrue(victim.hasSecret());
+//
+//        } catch (InvalidParameterException invalidPara){
+//            fail("Did not recognize a valid secret");
+//        }
+//    }
+//
+//    @Test
+//    void assignSecret_MorePegs() {
+//        initVictim();
+//        try{
+//            victim.assignSecret("Red", "Blue");
+//            assertTrue(false);
+////            assertTrue(victim.hasSecret());
+//
+//        } catch (InvalidParameterException invalidPara){
+//            fail("Did not recognize a valid secret");
+//        }
+//    }
+
     @Test
-    void assignSecret() {
-        initVictim();
-        victim.assignSecret();
+    void evaluateTest_GettingStarted() {
+        int[] evaluateResult = MasterMinder.evaluate(new String[] {"D", "F"},new String[] {"F", "G"});
+        assertNotNull(evaluateResult, "Return Something");
+        assertEquals(evaluateResult.length, 2, "Returns two numbers");
+//        evaluate([blue], [red]) should return [0, 0]
+//        evaluate([blue], [blue]) should return [1, 0]
+//        evaluate([red, yellow], [blue, red]) should return [0, 1]
+
     }
+
+    @Test
+    void evaluateTest_TestOnePegDoesNotMatch() {
+        int[] evaluateResult = MasterMinder.evaluate(new String[] {"blue"},new String[] {"red"});
+        assertNotNull(evaluateResult, "Return Something");
+        assertEquals(evaluateResult.length, 2, "Returns two numbers");
+        assertEquals(evaluateResult[0], 0, "Nothing well placed");
+        assertEquals(evaluateResult[1], 0, "Nothing well misplaced");
+    }
+
+    @Test
+    void evaluateTest_OnePegDoesMatch() {
+        int[] evaluateResult = MasterMinder.evaluate(new String[] {"blue"},new String[] {"blue"});
+        assertNotNull(evaluateResult, "Return Something");
+        assertEquals(evaluateResult.length, 2, "Returns two numbers");
+        assertEquals(evaluateResult[0], 1, "everything well placed");
+        assertEquals(evaluateResult[1], 0, "Nothing well misplaced");
+    }
+
+    @Test
+    void evaluateTest_TwoPegsOneMisplaced() {
+        int[] evaluateResult = MasterMinder.evaluate(new String[] {"red", "yellow"},new String[] {"blue", "red"});
+        assertNotNull(evaluateResult, "Return Something");
+        assertEquals(evaluateResult.length, 2, "Returns two numbers");
+        assertEquals(evaluateResult[0], 1, "everything well placed");
+        assertEquals(evaluateResult[1], 0, "Nothing well misplaced");
+    }
+
+    @Test
+    void evaluateTest_ProvidedTests() {
+//        evaluate([blue], [red]) should return [0, 0]
+//        evaluate([blue], [blue]) should return [1, 0]
+//        evaluate([red, yellow], [blue, red]) should return [0, 1]
+        fail("We have not done anything yet");
+    }
+
+
 }
